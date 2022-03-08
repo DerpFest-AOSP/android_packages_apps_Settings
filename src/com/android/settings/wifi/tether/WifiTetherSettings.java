715b9798a7077f4ccde01c05a41e376c295edad9
cjybyjk <cjybyjk@zjnu.edu.cn>
2022-03-09 00:35:16 +0800

    Settings: SoftAp: add support for setting shutdown timeout
    Change-Id: I80aebe3ca8e13e3dc5c503d5a8527282e96e486a
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: cjybyjk <cjybyjk@zjnu.edu.cn>

--- src/com/android/settings/wifi/tether/WifiTetherSettings.java
+++ src/com/android/settings/wifi/tether/WifiTetherSettings.java
@@ -348,8 +348,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         if (!mWifiTetherViewModel.isSpeedFeatureAvailable()) {
             mMaxCompatibilityPrefController.setupMaximizeCompatibility(configBuilder);
         }
-        configBuilder.setAutoShutdownEnabled(
-                mWifiTetherAutoOffPreferenceController.isEnabled());
+        mWifiTetherAutoOffPreferenceController.updateConfig(configBuilder);
         mClientPrefController.updateConfig(configBuilder);
         return configBuilder.build();
     }
@@ -361,6 +360,7 @@ public class WifiTetherSettings extends RestrictedDashboardFragment
         use(WifiTetherSecurityPreferenceController.class).updateDisplay();
         use(WifiTetherPasswordPreferenceController.class).updateDisplay();
         use(WifiTetherMaximizeCompatibilityPreferenceController.class).updateDisplay();
+        use(WifiTetherAutoOffPreferenceController.class).updateDisplay();
     }
 
     @Override
