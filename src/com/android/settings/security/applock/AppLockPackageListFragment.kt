c90ebb6bcc10f1c8304dc2f113c9fa57be2db0b7
Pranav Vashi <neobuddy89@gmail.com>
2023-03-04 22:07:20 +0530

    Settings: Update AppLock API
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- src/com/android/settings/security/applock/AppLockPackageListFragment.kt
+++ src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -29,7 +29,7 @@ import androidx.preference.Preference
 import androidx.preference.forEach
 
 import com.android.internal.logging.nano.MetricsProto
-import com.android.internal.util.custom.Utils
+import com.android.internal.util.clover.Utils
 
 import com.android.settings.R
 import com.android.settings.core.SubSettingLauncher
@@ -99,7 +99,11 @@ class AppLockPackageListFragment : DashboardFragment() {
 
     private suspend fun getSelectedPackages(): Set<String> {
         return withContext(Dispatchers.IO) {
-            appLockManager.packageData.map { it.packageName }.toSet()
+            appLockManager.packageData.filter {
+                it.shouldProtectApp == true
+            }.map {
+                it.packageName
+            }.toSet()
         }
     }
 
@@ -116,11 +120,7 @@ class AppLockPackageListFragment : DashboardFragment() {
             isChecked = isProtected
             setOnPreferenceChangeListener { _, newValue ->
                 lifecycleScope.launch(Dispatchers.IO) {
-                    if (newValue as Boolean) {
-                        appLockManager.addPackage(packageInfo.packageName)
-                    } else {
-                        appLockManager.removePackage(packageInfo.packageName)
-                    }
+                    appLockManager.setShouldProtectApp(packageInfo.packageName, newValue as Boolean)
                 }
                 return@setOnPreferenceChangeListener true
             }
