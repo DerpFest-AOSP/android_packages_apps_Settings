2161d51fe778eff537ec2443366ead8adabd76ab
Ido Ben-Hur <idoybh2@gmail.com>
2023-08-01 15:21:33 +0300

    Settings: Allow configuring haptics for wake gestures [2/2]
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/settings/gestures/PickupGestureInsidePreferenceController.java
+++ src/com/android/settings/gestures/PickupGestureInsidePreferenceController.java
@@ -17,14 +17,13 @@
 package com.android.settings.gestures;
 
 import android.content.Context;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
-import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.widget.MainSwitchPreference;
@@ -36,11 +35,14 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
 
     private static final String KEY = "gesture_pick_up";
     private static final String AMBIENT_KEY = "doze_pick_up_gesture_ambient";
+    private static final String VIB_KEY = "doze_pick_up_gesture_vibrate";
 
     private final boolean mDefault;
     private final Context mContext;
     private MainSwitchPreference mSwitch;
     private SecureSettingSwitchPreference mAmbientPref;
+    private SecureSettingSwitchPreference mVibPref;
+    private boolean mIsVibAvailable;
 
     public PickupGestureInsidePreferenceController(Context context) {
         super(context);
@@ -58,6 +60,7 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mAmbientPref = screen.findPreference(AMBIENT_KEY);
+        mVibPref = screen.findPreference(VIB_KEY);
         mSwitch = screen.findPreference(getPreferenceKey());
         mSwitch.setOnPreferenceClickListener(preference -> {
             final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
@@ -65,18 +68,24 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
             Settings.Secure.putInt(mContext.getContentResolver(),
                     Settings.Secure.DOZE_PICK_UP_GESTURE,
                     enabled ? 0 : 1);
-            updateAmbientEnablement(!enabled);
+            updateEnablement(!enabled);
             return true;
         });
         mSwitch.addOnSwitchChangeListener(this);
         updateState(mSwitch);
+
+        final Vibrator vibrator = mContext.getSystemService(Vibrator.class);
+        mIsVibAvailable = vibrator != null && vibrator.hasVibrator();
+        if (mVibPref != null && !mIsVibAvailable) {
+            mVibPref.setVisible(false);
+        }
     }
 
     public void setChecked(boolean isChecked) {
         if (mSwitch != null) {
             mSwitch.setChecked(isChecked);
         }
-        updateAmbientEnablement(isChecked);
+        updateEnablement(isChecked);
     }
 
     @Override
@@ -95,11 +104,15 @@ public class PickupGestureInsidePreferenceController extends AbstractPreferenceC
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
         Settings.Secure.putInt(mContext.getContentResolver(),
                 Settings.Secure.DOZE_PICK_UP_GESTURE, isChecked ? 1 : 0);
-        updateAmbientEnablement(isChecked);
+        updateEnablement(isChecked);
     }
 
-    private void updateAmbientEnablement(boolean enabled) {
-        if (mAmbientPref == null) return;
-        mAmbientPref.setEnabled(enabled);
+    private void updateEnablement(boolean enabled) {
+        if (mAmbientPref != null) {
+            mAmbientPref.setEnabled(enabled);
+        }
+        if (mVibPref != null && mIsVibAvailable) {
+            mVibPref.setEnabled(enabled);
+        }
     }
 }
--- src/com/android/settings/gestures/TapPreferenceController.java
+++ src/com/android/settings/gestures/TapPreferenceController.java
@@ -18,14 +18,13 @@ package com.android.settings.gestures;
 
 import android.content.Context;
 import android.os.SystemProperties;
+import android.os.Vibrator;
 import android.provider.Settings;
 import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceScreen;
 
-import com.android.settings.R;
 import com.android.settings.core.PreferenceControllerMixin;
 import com.android.settingslib.core.AbstractPreferenceController;
 import com.android.settingslib.widget.MainSwitchPreference;
@@ -37,10 +36,13 @@ public class TapPreferenceController extends AbstractPreferenceController
 
     private static final String KEY = "gesture_tap";
     private static final String AMBIENT_KEY = "doze_tap_gesture_ambient";
+    private static final String VIB_KEY = "doze_tap_gesture_vibrate";
 
     private final Context mContext;
     private MainSwitchPreference mSwitch;
     private SecureSettingSwitchPreference mAmbientPref;
+    private SecureSettingSwitchPreference mVibPref;
+    private boolean mIsVibAvailable;
 
     public TapPreferenceController(Context context) {
         super(context);
@@ -56,6 +58,7 @@ public class TapPreferenceController extends AbstractPreferenceController
     public void displayPreference(PreferenceScreen screen) {
         super.displayPreference(screen);
         mAmbientPref = screen.findPreference(AMBIENT_KEY);
+        mVibPref = screen.findPreference(VIB_KEY);
         mSwitch = screen.findPreference(getPreferenceKey());
         mSwitch.setOnPreferenceClickListener(preference -> {
             final boolean enabled = Settings.Secure.getInt(mContext.getContentResolver(),
@@ -63,18 +66,24 @@ public class TapPreferenceController extends AbstractPreferenceController
             Settings.Secure.putInt(mContext.getContentResolver(),
                     Settings.Secure.DOZE_TAP_SCREEN_GESTURE,
                     enabled ? 0 : 1);
-            updateAmbientEnablement(!enabled);
+            updateEnablement(!enabled);
             return true;
         });
         mSwitch.addOnSwitchChangeListener(this);
         updateState(mSwitch);
+
+        final Vibrator vibrator = mContext.getSystemService(Vibrator.class);
+        mIsVibAvailable = vibrator != null && vibrator.hasVibrator();
+        if (mVibPref != null && !mIsVibAvailable) {
+            mVibPref.setVisible(false);
+        }
     }
 
     public void setChecked(boolean isChecked) {
         if (mSwitch != null) {
             mSwitch.setChecked(isChecked);
         }
-        updateAmbientEnablement(isChecked);
+        updateEnablement(isChecked);
     }
 
     @Override
@@ -94,11 +103,15 @@ public class TapPreferenceController extends AbstractPreferenceController
         Settings.Secure.putInt(mContext.getContentResolver(),
                 Settings.Secure.DOZE_TAP_SCREEN_GESTURE, isChecked ? 1 : 0);
         SystemProperties.set("persist.sys.tap_gesture", isChecked ? "1" : "0");
-        updateAmbientEnablement(isChecked);
+        updateEnablement(isChecked);
     }
 
-    private void updateAmbientEnablement(boolean enabled) {
-        if (mAmbientPref == null) return;
-        mAmbientPref.setEnabled(enabled);
+    private void updateEnablement(boolean enabled) {
+        if (mAmbientPref != null) {
+            mAmbientPref.setEnabled(enabled);
+        }
+        if (mVibPref != null && mIsVibAvailable) {
+            mVibPref.setEnabled(enabled);
+        }
     }
 }
