fe4f089d73904f08b6482d429e581de94874b531
cjh1249131356 <cjh1249131356@gmail.com>
2024-01-06 22:36:19 +0800

    Settings: Implement RefreshRateManager [2/2]
    Includes:
    - Per-app refresh rate config
    - Extreme refresh rate (Force all apps to run in maximum refresh rate)
    - Temp refresh rate allowed (Used in MEMC)
    - Refresh rate QS tile
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -22,6 +22,7 @@ import com.android.settings.MainClear;
 import com.android.settings.MainClearConfirm;
 import com.android.settings.ResetNetwork;
 import com.android.settings.Settings;
+import com.android.settings.Settings.ScreenRefreshRateActivity;
 import com.android.settings.TestingSettings;
 import com.android.settings.TrustedCredentialsSettings;
 import com.android.settings.accessibility.AccessibilityDetailsSettingsFragment;
@@ -223,6 +224,7 @@ import com.android.settings.wifi.details.WifiNetworkDetailsFragment;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import org.derpfest.settings.display.refreshrate.ScreenRefreshRateFragment;
 
 import org.derpfest.customizations.DerpFestCustomizations;
 
@@ -439,6 +441,7 @@ public class SettingsGateway {
             SafetyCenterFragment.class.getName(),
             RunningServices.class.getName(),
             PrivateDnsSettings.class.getName(),
+            ScreenRefreshRateFragment.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -490,5 +493,6 @@ public class SettingsGateway {
             Settings.DerpFestCustomizationsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.PrivateDnsSettingsActivity.class.getName(),
+            Settings.ScreenRefreshRateActivity.class.getName(),
     };
 }
