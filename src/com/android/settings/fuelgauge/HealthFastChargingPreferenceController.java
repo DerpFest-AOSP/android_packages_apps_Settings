1493564dea0a74d11728e80860a0e9dae1cd9ce6
LuK1337 <priv.luk@gmail.com>
2025-08-14 01:28:38 +0200

    Settings: Add lineagehealth IFastCharge interface support
    Change-Id: If5b88efde3bf71e6c38ee0e6ac812ed1a3287b13

---
+++ src/com/android/settings/fuelgauge/HealthFastChargingPreferenceController.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.fuelgauge;
+
+import android.content.Context;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.settings.core.BasePreferenceController;
+
+import com.google.common.primitives.Ints;
+
+import lineageos.health.HealthInterface;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Controller to change and update the fast charging settings
+ */
+public class HealthFastChargingPreferenceController extends BasePreferenceController
+        implements Preference.OnPreferenceChangeListener {
+
+    private static final String TAG = "HealthFastChargingPreferenceController";
+
+    private HealthInterface mHealthInterface;
+    private ListPreference mListPreference;
+
+    public HealthFastChargingPreferenceController(Context context, String key) {
+        super(context, key);
+
+        try {
+            mHealthInterface = HealthInterface.getInstance(context);
+        } catch (RuntimeException e) {
+            // Not supported.
+        }
+    }
+
+    @Override
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+
+        mListPreference = screen.findPreference(getPreferenceKey());
+
+        if (mHealthInterface != null && mHealthInterface.isFastChargeSupported()) {
+            List<CharSequence> entries = Arrays.asList(mListPreference.getEntries());
+            List<CharSequence> entryValues = Arrays.asList(mListPreference.getEntryValues());
+            int[] supportedFastChargeModes = mHealthInterface.getSupportedFastChargeModes();
+
+            for (int i = entryValues.size() - 1; i >= 0; i--) {
+                int fastChargeMode = Integer.parseInt(entryValues.get(i).toString());
+                if (!Ints.contains(supportedFastChargeModes, fastChargeMode)) {
+                    entries.remove(i);
+                    entryValues.remove(i);
+                }
+            }
+
+            mListPreference.setEntries(entries.toArray(new CharSequence[entries.size()]));
+            mListPreference.setEntryValues(entryValues.toArray(new CharSequence[entryValues.size()]));
+        }
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mHealthInterface != null && mHealthInterface.isFastChargeSupported()
+                ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
+    }
+
+    @Override
+    public void updateState(Preference preference) {
+        String fastChargeMode = String.valueOf(mHealthInterface.getFastChargeMode());
+        int index = mListPreference.findIndexOfValue(fastChargeMode);
+        if (index < 0) index = 0;
+        mListPreference.setValueIndex(index);
+        mListPreference.setSummary(mListPreference.getEntries()[index]);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (mHealthInterface.setFastChargeMode(Integer.parseInt((String) newValue))) {
+            updateState(preference);
+            return true;
+        }
+
+        return false;
+    }
+}
