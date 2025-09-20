3130a8e7d56e912066111a92454c38300f5b5e04
NurKeinNeid <nurkeinneid@derpfest.org>
2025-09-20 19:08:16 +0200

    Settings: Skip theming for AOSP default icons
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -380,6 +380,11 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     }
 
     private void themePreference(Preference pref) {
+        // Skip theming entirely for case 0 (AOSP default)
+        if (mIconStyle == 0) {
+            return;
+        }
+        
         Drawable icon = pref.getIcon();
         if (icon != null) {
             if (icon instanceof AdaptiveIcon) {
@@ -387,9 +392,6 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 // Clear colors from previous calls
                 aIcon.resetCustomColors();
                 switch (mIconStyle) {
-                    case 0:
-                    default:
-                        break;
                     case 1:
                         aIcon.setCustomForegroundColor(getResources().getColor(android.R.color.white));
                         break;
@@ -414,9 +416,6 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                     bg.setTintList(null);
                     fg.setTintList(null);
                     switch (mIconStyle) {
-                        case 0:
-                        default:
-                            break;
                         case 1:
                             fg.setTint(getResources().getColor(android.R.color.white));
                             break;
