71ce94475e7a7eb86b895ca4216734801797983a
HeroBuxx <me@herobuxx.me>
2026-07-09 16:22:02 +0000

    Settings: Always show RGB adjustment
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/settings/display/ColorBalancePreferenceController.java
+++ src/com/android/settings/display/ColorBalancePreferenceController.java
@@ -40,8 +40,7 @@ public class ColorBalancePreferenceController extends SliderPreferenceController
 
     @Override
     public int getAvailabilityStatus() {
-        return ColorDisplayManager.isColorTransformAccelerated(mContext) ?
-                AVAILABLE : UNSUPPORTED_ON_DEVICE;
+        return AVAILABLE;
     }
 
     @Override
